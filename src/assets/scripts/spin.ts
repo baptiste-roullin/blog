6e35314b39de6ab2eca3fda053c7c4d207bee0cd
Your Name <you@example.com>
2021-10-11 03:32:48 +0200

    fix spin

--- src/assets/scripts/spin.ts
+++ src/assets/scripts/spin.ts
@@ -1,11 +1,12 @@
 export default function spin() {
 
-	document.querySelector('#site-title')!.addEventListener('mouseover', function (e) {
+	document.querySelector('#site-title-text')!.addEventListener('mouseover', function (e) {
 		const target = e.target! as HTMLElement
-		if (!(target.querySelector('#site-title-text span'))) {
 
-			target.querySelector('#site-title-text')!.innerHTML = 'Tout ce qui b<span>o</span>uge'
-			const o = target.querySelector('a span')! as HTMLElement
+		if (target.tagName !== "SPAN") {
+
+			target.innerHTML = 'Tout ce qui b<span>o</span>uge'
+			const o = target.querySelector(' span')! as HTMLElement
 			o.offsetWidth;
 
 			o.classList.add('spin')
