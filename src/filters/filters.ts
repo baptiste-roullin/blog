ded8fed5bb84c45ce2c7e08d544898160e41650f
Saint-loup <shakespearien@gmail.com>
2021-10-25 15:47:15 +0200

    shuffle les features en home

--- src/filters/filters.ts
+++ src/filters/filters.ts
@@ -83,6 +83,26 @@ module.exports = {
 		return arr.slice(a, b);
 	},
 
+	shuffle: function (array) {
+
+		var currentIndex = array.length;
+		var temporaryValue, randomIndex;
+
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// And swap it with the current element.
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+		return array;
+	},
+
 	searchIndex: (process.env.SEARCH === "false" ? () => "[RECHERCHE DÉSACTIVÉE]" : search)
 	,
 
