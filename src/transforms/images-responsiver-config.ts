e44849155998965cf5d1dff5044d94a047f09ac7
Baptiste <baptiste@roullin.net>
2021-11-14 16:41:41 +0100

    créer fonction

--- src/transforms/images-responsiver-config.ts
+++ src/transforms/images-responsiver-config.ts
@@ -37,66 +37,96 @@ module.exports = {
 					/^(.*)(\.[^\.]+)$/,
 					'$1-' + width + '.jpg')
 		},
-		runBefore: async (image, document) => {
-			let originalPath = normalizePath(image.getAttribute('src'))
-			const intermediaryPath = "src/assets/imagesToProcess/" + path.basename(originalPath)
-
-			try {
-				const imageDimensions = imageSize(intermediaryPath);
-				image.setAttribute('width', imageDimensions.width);
-				image.setAttribute('height', imageDimensions.height);
-
-				const options = {
-					sharpWebpOptions: {
-						quality: 90,
-					},
-					widths: [360, 750, imageDimensions.width, 1140, 1530, 1920],
-					dryRun: false,
-					formats: formats,
-					urlPath: '/assets/imagesToProcess/',
-					outputDir: './dist/assets/generatedImages/',
-					filenameFormat: function (id, src, width, format, options) {
-						const extension = path.extname(src);
-						const name = path.basename(src, extension);
-						const modifiedFormat = (format === 'jpeg' ? 'jpg' : format);
-						return `${name}-${width}.${modifiedFormat}`;
-					}
-				}
-
-				/*		const exists = promisify(require("fs").exists);
-						if (!(await exists(intermediaryPath))) {
-							console.log(intermediaryPath + 'debug : existe pas')
-						}*/
-				await transformPicture(intermediaryPath, options);
-
-				image.dataset.responsiver = image.className;
-				//image.dataset.responsiveruRL = metadata.jpg.url;
-				image.dataset.size = image.className;
-
-			}
-			catch (e) {
-				console.log("debug:    " + originalPath + "  " + e)
-			}
-		},
-		runAfter: (image, document) => {
-			//image.setAttribute('src', image.dataset.responsiveruRL);
-			//let caption = image.getAttribute("title");
-			if (image.closest('.rich-picture')) {
-				const link = document.createElement("a");
-				link.setAttribute("data-pswp-srcset", image.getAttribute('srcset'));
-
-				link.setAttribute("href", image.getAttribute('src'));
-				link.appendChild(image.cloneNode(true));
-				link.setAttribute('data-pswp-width', image.width);
-				link.setAttribute('data-pswp-height', image.height);
-				image.replaceWith(link);
-
-			}
-		},
+		runBefore: runBefore,
+		runAfter: runAfter,
 		steps: 5,
 		classes: ['img-default'],
 		attributes: { loading: 'lazy', },
 	},
-	gallery_3x2: {
-	},
+	/*
+		"post-list": {
+			selector: " .post-list :not(picture)  > img[src]:not([srcset]):not([src$='.svg']):not([src$='.gif'])",
+			minWidth: 420,
+			maxWidth: 420,
+			fallbackWidth: 420,
+			sizes: '(max-width: 60rem) 90vw, 60rem',
+			resizedImageUrl: (src, width) => {
+				if (!(new RegExp('^/').test(src)) || src !== "") {
+					src = "/assets/generatedImages/" + src
+				}
+				return src.
+					replace(
+						/\/assets\/.*\//,
+						'/assets/generatedImages/').
+					replace(
+						/^(.*)(\.[^\.]+)$/,
+						'$1-' + width + '.jpg')
+			},
+			runBefore: runBefore,
+			runAfter: runAfter,
+			steps: 5,
+			classes: ['img-default'],
+			attributes: { loading: 'lazy', },
+		}*/
+}
+
+
+
+
+function runAfter(image, document) {
+	//image.setAttribute('src', image.dataset.responsiveruRL);
+	//let caption = image.getAttribute("title");
+	if (image.closest('.rich-picture')) {
+		const link = document.createElement("a");
+		link.setAttribute("data-pswp-srcset", image.getAttribute('srcset'));
+
+		link.setAttribute("href", image.getAttribute('src'));
+		link.appendChild(image.cloneNode(true));
+		link.setAttribute('data-pswp-width', image.width);
+		link.setAttribute('data-pswp-height', image.height);
+		image.replaceWith(link);
+	}
+}
+
+
+async function runBefore(image, document) {
+	let originalPath = normalizePath(image.getAttribute('src'))
+	const intermediaryPath = "src/assets/imagesToProcess/" + path.basename(originalPath)
+
+	try {
+		const imageDimensions = imageSize(intermediaryPath);
+		image.setAttribute('width', imageDimensions.width);
+		image.setAttribute('height', imageDimensions.height);
+
+		const options = {
+			sharpWebpOptions: {
+				quality: 90,
+			},
+			widths: [360, 750, imageDimensions.width, 1140, 1530, 1920],
+			dryRun: false,
+			formats: formats,
+			urlPath: '/assets/imagesToProcess/',
+			outputDir: './dist/assets/generatedImages/',
+			filenameFormat: function (id, src, width, format, options) {
+				const extension = path.extname(src);
+				const name = path.basename(src, extension);
+				const modifiedFormat = (format === 'jpeg' ? 'jpg' : format);
+				return `${name}-${width}.${modifiedFormat}`;
+			}
+		}
+
+		/*		const exists = promisify(require("fs").exists);
+				if (!(await exists(intermediaryPath))) {
+					console.log(intermediaryPath + 'debug : existe pas')
+				}*/
+		await transformPicture(intermediaryPath, options);
+
+		image.dataset.responsiver = image.className;
+		//image.dataset.responsiveruRL = metadata.jpg.url;
+		image.dataset.size = image.className;
+
+	}
+	catch (e) {
+		console.log("debug:    " + originalPath + "  " + e)
+	}
 }
