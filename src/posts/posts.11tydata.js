f6f7001500aa1cbcf12e7e812d4ed2819630c8d0
Baptiste <baptiste@roullin.net>
2022-01-01 21:55:22 +0100

    simplifié flot

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -13,24 +13,15 @@ module.exports = {
     collatedHeroImage: async (data) => {
       const slug = data.page.fileSlug
       const hero = data.hero
-      if (data.draft !== true) {
-        if (hero === undefined || hero === {}) {
-          await truchetNode(slug, 400, 280).catch(console.error);
-          return `truchet-${slug}.png`
-        }
-        else {
-          const isGif = /\.gif$/
-          if (isGif.test(hero.image)) {
-            return "/assets/generatedImages/" + hero.image
 
-          }
-          else {
-            return hero.image
-          }
-        }
+      if (hero === undefined || hero === {}) {
+        await truchetNode(slug, 400, 280).catch(console.error);
+        return `truchet-${slug}.png`
       }
-      return undefined
-    },
+      else {
+        return hero.image
+      }
 
+    },
   }
 };
