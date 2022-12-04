1066ad50df91c1a65e3339271e6dbdca8c9ba171
Baptiste <baptiste@roullin.net>
2022-12-04 23:07:27 +0100

    doc

--- src/_data/threads.js
+++ src/_data/threads.js
@@ -4,15 +4,9 @@ import threads from '../pages/threads/threader'
 module.exports = (meta.twitterThread === "false" ? ["feature disabled"] : threads)
 
 
-//todo : si tweet contient uniquement QT : vider tweet ou remplacer par QT
-//too : remplacer br par \n\n
+//todo : indenter QT
 //TODO : arrêter les requêtes si tweet_id = tweet déjà en cache (pas juste le tout premier tweet)
-//TODO : if lien externe générer card.
-//TODO : if quote tweet : suivre lien et générer card. les données sont là, manque  le template.
-//TODO : Acces à la liste quelque part dans la nav ou le site.
 //TODO : remonter la chaine uniquement si author=saint_loup
-//TODO : design
 //TODO : compteur de requêtes pour ne jamais dépasser le rate limit
-//TODO : fetchTwitter devrait retourner du json avec plein d'infos (id, etc.)
 //TODO : card twitter dans <head>
-//TODO : Héberger images
+//TODO : Ré-héberger images
