ec5ce1b4513f6aaa1312c98efd531c3ac896e038
Baptiste <baptiste@roullin.net>
2022-12-05 04:24:49 +0100

    update

--- src/_data/threads.js
+++ src/_data/threads.js
@@ -5,6 +5,7 @@ module.exports = (meta.twitterThread === "false" ? ["feature disabled"] : thread
 
 
 //todo : indenter QT
+//todo : supprimer ou améliorer "message vide"
 //TODO : arrêter les requêtes si tweet_id = tweet déjà en cache (pas juste le tout premier tweet)
 //TODO : remonter la chaine uniquement si author=saint_loup
 //TODO : compteur de requêtes pour ne jamais dépasser le rate limit
