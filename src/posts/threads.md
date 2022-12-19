8bb6a14f8ef82b617a58e3a73ef780a4f57a5461
Baptiste <baptiste@roullin.net>
2022-12-19 03:57:18 +0100

    post

---
+++ src/posts/threads.md
@@ -0,0 +1,19 @@
+# Archiver des fils Twitter : panorama des possibilités
+
+
+
+Pour archiver des fils de votre compte Twitter, il y a plusieurs possibilités.
+
+Pour exporter un **fil récent**, on peut [utiliser cet outil en ligne](https://threadreaderapp.com/), très simple puisqu'il suffit d'entrer l'URL d'un tweet. Mais ne marche pas avec de vieux messages.
+
+Avec **un export officiel**, avec on peut utiliser [outil un peu compliqué](https://github.com/tweetback/tweetback/) ou [cet outil plus simple](https://tinysubversions.com/twitter-archive/make-your-own/). Ils créent un mini-site web avec tous ses tweets, issus de fils ou non.
+
+Il y a cette [expérimentation en ligne](https://social.perma.cc/#why-faq) qui simule un navigateur scrollant Twitter pour **générer un PDF**, à des fins de notarisation et de preuve d'authenticité.
+
+Et à ma connaissance, c'est tout. A cause des limites de l'API, il n'existe pas d'outils respectant les contraintes suivant :
+
+1. Pas besoin de télécharger l'export officiel si le fil évolue
+2. Marche avec des vieux threads ou des threads couvrant une longue période
+3. Marche aussi bien à partir du premier tweet d'un fil que du dernier.
+
+Du coup 
--- src/transforms/pictures_static.ts
+++ src/transforms/pictures_static.ts
@@ -1,9 +1,10 @@
+//TODO : images pas mises en cache HTTP.
 
 require('dotenv').config()
-const convertPicturesLibrary = require("@11ty/eleventy-img");
-import path from "path";
+const convertPicturesLibrary = require("@11ty/eleventy-img")
+import path from "path"
 const meta = require('../_data/meta.js')
-const clonedeep = require('lodash.clonedeep');
+const clonedeep = require('lodash.clonedeep')
 
 
 function normalizePath(str) {
@@ -29,7 +30,7 @@ export const globalSettings = {
 	attributes: { loading: 'lazy', },
 	ignore: 'truchet-'
 
-};
+}
 
 
 
@@ -43,10 +44,10 @@ function convertPictures(image, document) {
 	try {
 		// TODO : Tester cache. Par exemple "truchet-interet legitime.jpg" est-il mis en cache une seule fois.
 
-		const imageDimensions = convertPicturesLibrary.statsSync(intermediaryPath, { statsOnly: true, formats: ["webp"] });
+		const imageDimensions = convertPicturesLibrary.statsSync(intermediaryPath, { statsOnly: true, formats: ["webp"] })
 
-		image.setAttribute('width', imageDimensions.webp[0].width);
-		image.setAttribute('height', imageDimensions.webp[0].height);
+		image.setAttribute('width', imageDimensions.webp[0].width)
+		image.setAttribute('height', imageDimensions.webp[0].height)
 
 
 		const options = {
@@ -66,17 +67,17 @@ function convertPictures(image, document) {
 			urlPath: '/assets/imagesToProcess/',
 			outputDir: `./${meta.outputDir}/${meta.assetsDir}/`,
 			filenameFormat: function (id, src, width, format, options) {
-				const extension = path.extname(src);
-				const name = path.basename(src, extension);
-				const modifiedFormat = (format === 'jpeg' ? 'jpg' : format);
-				return `${name}-${width}.${modifiedFormat}`;
+				const extension = path.extname(src)
+				const name = path.basename(src, extension)
+				const modifiedFormat = (format === 'jpeg' ? 'jpg' : format)
+				return `${name}-${width}.${modifiedFormat}`
 			}
 		}
-		convertPicturesLibrary(intermediaryPath, options);
+		convertPicturesLibrary(intermediaryPath, options)
 
-		image.dataset.responsiver = image.className;
+		image.dataset.responsiver = image.className
 		//image.dataset.responsiveruRL = metadata.jpg.url;
-		image.dataset.size = image.className;
+		image.dataset.size = image.className
 
 	}
 	catch (e) {
@@ -89,32 +90,32 @@ export function prepareForLighbox(image, document) {
 	//image.setAttribute('src', image.dataset.responsiveruRL);
 	//let caption = image.getAttribute("title");
 	if (image.closest('.rich-picture')) {
-		const link = document.createElement("a");
-		link.setAttribute("data-pswp-srcset", image.getAttribute('srcset'));
+		const link = document.createElement("a")
+		link.setAttribute("data-pswp-srcset", image.getAttribute('srcset'))
 
-		link.setAttribute("href", image.getAttribute('src'));
-		link.appendChild(image.cloneNode(true));
-		link.setAttribute('data-pswp-width', image.width);
-		link.setAttribute('data-pswp-height', image.height);
-		image.replaceWith(link);
+		link.setAttribute("href", image.getAttribute('src'))
+		link.appendChild(image.cloneNode(true))
+		link.setAttribute('data-pswp-width', image.width)
+		link.setAttribute('data-pswp-height', image.height)
+		image.replaceWith(link)
 	}
 }
 
 export function handlePictures(image, document, globalSettings) {
 
-	let imageSettings = clonedeep(globalSettings);
-	convertPictures(image, document);
+	let imageSettings = clonedeep(globalSettings)
+	convertPictures(image, document)
 
-	const imageSrc = image.getAttribute('src') as string;
+	const imageSrc = image.getAttribute('src') as string
 	//console.log(`Transforming ${imageSrc}`);
 
-	const imageWidth = image.getAttribute('width');
+	const imageWidth = image.getAttribute('width')
 
 	if (imageWidth === null) {
 		//console.log(`The image should have a width attribute: ${imageSrc}`);
 	}
 
-	let srcsetList: string[] = [];
+	let srcsetList: string[] = []
 	if (
 		imageSettings.widthsList !== undefined &&
 		imageSettings.widthsList.length > 0
@@ -155,38 +156,38 @@ export function handlePictures(image, document, globalSettings) {
 		// Make sure there are at least 2 steps for minWidth and maxWidth
 		if (imageSettings.steps < 2) {
 			//console.log(`Steps should be >= 2: ${imageSettings.steps} step for ${imageSrc}`);
-			imageSettings.steps = 2;
+			imageSettings.steps = 2
 		}
 
 		// Make sure maxWidth > minWidth
 		// (even if there would be no issue in `srcset` order)
 		if (imageSettings.minWidth > imageSettings.maxWidth) {
 			//console.log(`Combined options have minWidth > maxWidth for ${imageSrc}`);
-			let tempMin = imageSettings.minWidth;
-			imageSettings.minWidth = imageSettings.maxWidth;
-			imageSettings.maxWidth = tempMin;
+			let tempMin = imageSettings.minWidth
+			imageSettings.minWidth = imageSettings.maxWidth
+			imageSettings.maxWidth = tempMin
 		}
 
 		if (imageWidth !== null) {
 			if (imageWidth < imageSettings.minWidth) {
 				//console.log(`The image is smaller than minWidth: ${imageWidth} < ${imageSettings.minWidth}`);
-				imageSettings.minWidth = imageWidth;
+				imageSettings.minWidth = imageWidth
 			}
 			if (imageWidth < imageSettings.fallbackWidth) {
 				//console.log(`The image is smaller than fallbackWidth: ${imageWidth} < ${imageSettings.fallbackWidth}`);
-				imageSettings.fallbackWidth = imageWidth;
+				imageSettings.fallbackWidth = imageWidth
 			}
 		}
 
 		// generate the srcset attribute
-		let previousStepWidth = 0;
+		let previousStepWidth = 0
 		for (let i = 0; i < imageSettings.steps; i++) {
 			let stepWidth = Math.ceil(
 				imageSettings.minWidth +
 				((imageSettings.maxWidth - imageSettings.minWidth) /
 					(imageSettings.steps - 1)) *
 				i
-			);
+			)
 
 			if (imageWidth !== null && stepWidth >= imageWidth) {
 				//console.log(`The image is smaller than maxWidth: ${imageWidth} < ${imageSettings.maxWidth}`);
@@ -195,49 +196,49 @@ export function handlePictures(image, document, globalSettings) {
 						imageSrc,
 						imageWidth
 					)} ${imageWidth}w`
-				);
-				break;
+				)
+				break
 			}
 			if (stepWidth === previousStepWidth) {
 				// Don't set twice the same image width
-				continue;
+				continue
 			}
-			previousStepWidth = stepWidth;
+			previousStepWidth = stepWidth
 			srcsetList.push(
 				`${imageSettings.resizedImageUrl(
 					imageSrc,
 					stepWidth
 				)} ${stepWidth}w`
-			);
+			)
 		}
 	}
 
 	if (imageSettings.classes.length > 0) {
-		image.classList.add(...imageSettings.classes);
+		image.classList.add(...imageSettings.classes)
 	}
 
 	// Change the image source
 	image.setAttribute(
 		'src',
 		imageSettings.resizedImageUrl(imageSrc, imageSettings.fallbackWidth)
-	);
+	)
 
-	image.setAttribute('srcset', srcsetList.join(', '));
+	image.setAttribute('srcset', srcsetList.join(', '))
 
 	// add sizes attribute
-	image.setAttribute('sizes', imageSettings.sizes);
+	image.setAttribute('sizes', imageSettings.sizes)
 
 	// add 'data-pristine' attribute with URL of the pristine image
-	image.dataset.pristine = imageSrc;
+	image.dataset.pristine = imageSrc
 
 	// Add attributes from the preset
 	if (Object.keys(imageSettings.attributes).length > 0) {
 		for (const attribute in imageSettings.attributes) {
 			if (imageSettings.attributes[attribute] !== null) {
-				image.setAttribute(attribute, imageSettings.attributes[attribute]);
+				image.setAttribute(attribute, imageSettings.attributes[attribute])
 			}
 		}
 	}
 
-	prepareForLighbox(image, document);
+	prepareForLighbox(image, document)
 }
