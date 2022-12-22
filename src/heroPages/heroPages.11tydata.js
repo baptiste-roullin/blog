32e89defca416a1f6fba0e8641748b3ff30c3a01
Baptiste <baptiste@roullin.net>
2022-12-22 23:20:11 +0100

    update

--- src/heroPages/heroPages.11tydata.js
+++ src/heroPages/heroPages.11tydata.js
@@ -3,6 +3,7 @@ import collateHeroImage from '../utils/collateHeroImage'
 
 module.exports = {
   layout: "base",
+  hideMeta: true,
   "permalink": "/{{page.fileSlug}}/index.html",
   author: "{{ meta.author }}",
   eleventyComputed: {
