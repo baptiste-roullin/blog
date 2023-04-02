c135651317e3b34a33ff90f5e9f61414f2bc0361
Baptiste <baptiste@roullin.net>
2023-04-02 15:22:31 +0200

    ajout try catch

--- src/heroPages/threads/threader_core.ts
+++ src/heroPages/threads/threader_core.ts
@@ -66,16 +66,20 @@ export default async function threader(author_id: string, token: string | undefi
 	// alternative moins complète : requêter le tweet qui cite avec :
 	//"expansions": ["referenced_tweets.id"],
 	async function getQT(tweetID, Client): Promise<Tweet | undefined> {
-		const response = await Client.tweets.findTweetById(
-			tweetID, fields
-		)
+		try {
+			const response = await Client.tweets.findTweetById(tweetID, fields)
 
-		if (response.errors) {
-			error(new Error(JSON.stringify(response.errors[0])))
-		}
-		else if (response.data) {
-			//info("QT fetché", response.data.text)
-			return Object.assign(response.data, response?.includes)
+			if (response.errors) {
+				error(new Error(JSON.stringify(response.errors[0])))
+			}
+			else if (response.data) {
+				//info("QT fetché", response.data.text)
+				return Object.assign(response.data, response?.includes)
+			}
+
+		} catch (error) {
+			console.log(error)
+
 		}
 	}
 
@@ -93,7 +97,6 @@ export default async function threader(author_id: string, token: string | undefi
 						return { url: url, title: "Lien vers la ressource" }
 					}
 					const html = await response.text()
-					//info(response.url, response.headers)
 					const metadata = await metascraper({ url: url, html: html })
 					if (!metadata.url.includes("https://twitter.com")) {
 						return metadata
@@ -112,87 +115,93 @@ export default async function threader(author_id: string, token: string | undefi
 	}
 	// recursive function
 	async function getTweet(thread: Thread, tweets: Tweet[], client: Client, cachedThread) {
+		try {
 
-		const response = await client.tweets.findTweetById(thread.tweetID, fields)
+			const response = await client.tweets.findTweetById(thread.tweetID, fields)
 
-		if (response.errors) {
-			error(new Error(JSON.stringify(response.errors[0])))
-			info(thread.title)
-			return thread
-		}
-		else if (response.data) {
-			info("tweet fetché", response.data.text)
-
-			// les données intéressants sont dans deux clés. On aplatit pour avoir unseul objet.
-			let tweet: Tweet = Object.assign(response.data, response?.includes)
-
-
-			// Threader ne fonctionne qu'avec les threads du compte Twitter passé en paramètre
-			// Scraping ok mais y a des limites.
-			if (tweet.author_id !== author_id) {
+			if (response.errors) {
+				error(new Error(JSON.stringify(response.errors[0])))
+				info(thread.title)
 				return thread
 			}
+			else if (response.data) {
+				warning("tweet fetché", response.data.text)
 
-			// GESTION DES URLS DANS LE TWEET
-			const urls = response.data.text.match(url_catcher)
-			if (urls !== null) {
-				// les tweets avec photo incluent l'URL de la photo. On évite de générer un aperçu de cette URL.
-				if (
-					// Cas tweet vide
-					!(urls[0].length === response.data.text.length && "attachments" in response.data)
-				) {
-					const links = await generateCard(urls, tweet) as Links[]
-					if (links) {
-						tweet.linksMetadata = links.filter(
-							link => !(!link.title && /^https:\/\/t\.co/.test(link.url))
-						)
+				// les données intéressants sont dans deux clés. On aplatit pour avoir unseul objet.
+				let tweet: Tweet = Object.assign(response.data, response?.includes)
+
+
+				// Threader ne fonctionne qu'avec les threads du compte Twitter passé en paramètre
+				// Scraping ok mais y a des limites.
+				if (tweet.author_id !== author_id) {
+					return thread
+				}
+
+				// GESTION DES URLS DANS LE TWEET
+				const urls = response.data.text.match(url_catcher)
+				if (urls !== null) {
+					// les tweets avec photo incluent l'URL de la photo. On évite de générer un aperçu de cette URL.
+					if (
+						// Cas tweet vide
+						!(urls[0].length === response.data.text.length && "attachments" in response.data)
+					) {
+						const links = await generateCard(urls, tweet) as Links[]
+						if (links) {
+							tweet.linksMetadata = links.filter(
+								link => !(!link.title && /^https:\/\/t\.co/.test(link.url))
+							)
+						}
 					}
 				}
-			}
 
-			let referenced_tweets = response.data?.referenced_tweets
-			tweet.QTList = []
+				let referenced_tweets = response.data?.referenced_tweets
+				tweet.QTList = []
 
 
-			// RÉCURSION POUR REMONTER LE THREADER
-			// Si ce tweet réfère à d'autres tweets, c'est un thread.
-			if (referenced_tweets) {
-				addTweet(tweets, tweet)
+				// RÉCURSION POUR REMONTER LE THREADER
+				// Si ce tweet réfère à d'autres tweets, c'est un thread.
+				if (referenced_tweets) {
+					addTweet(tweets, tweet)
 
-				// Il peut y avoir tweets référencés. On distingue les citations et les réponses.
-				for await (const referenced_tweet of referenced_tweets) {
-					thread.tweetID = referenced_tweet.id
-					switch (referenced_tweet.type) {
-						// Si réponse, on remonte le thread en appelant à nouveau getTweet()
-						case "replied_to":
-							await scheduler.wait(options.delay)
-							info("the thread continues")
-							return await getTweet(thread, tweets, client, cachedThread)
-						// Si c'est une citation, on récupère le QT et on l'ajoute dans une clé dédiée de l'objet Tweet.
-						case 'quoted':
-							const QT = await getQT(thread.tweetID, Client)
-							if (QT) {
-								tweet.QTList.push(QT!)
-							}
-							break
-						default:
-							break
+					// Il peut y avoir tweets référencés. On distingue les citations et les réponses.
+					for await (const referenced_tweet of referenced_tweets) {
+						thread.tweetID = referenced_tweet.id
+						switch (referenced_tweet.type) {
+							// Si réponse, on remonte le thread en appelant à nouveau getTweet()
+							case "replied_to":
+								await scheduler.wait(options.delay)
+								info("the thread continues")
+								return await getTweet(thread, tweets, client, cachedThread)
+							// Si c'est une citation, on récupère le QT et on l'ajoute dans une clé dédiée de l'objet Tweet.
+							case 'quoted':
+								const QT = await getQT(thread.tweetID, Client)
+								if (QT) {
+									tweet.QTList.push(QT!)
+								}
+								break
+							default:
+								break
+						}
 					}
-				}
-			} else {
-				if (tweets.length === 1) {
-					warning("This tweet doesn't answer to another tweet. Are you sure this is a thread?")
+				} else {
+					if (tweets.length === 1) {
+						warning("This tweet doesn't answer to another tweet. Are you sure this is a thread?")
+					}
+
+					info("found end of thread")
+					addTweet(tweets, tweet)
+					thread.tweets = tweets
+					await cachedThread.save(thread, "json")
+
+					return thread
 				}
 
-				info("found end of thread")
-				addTweet(tweets, tweet)
-				thread.tweets = tweets
-				await cachedThread.save(thread, "json")
-
-				return thread
 			}
+		} catch (error) {
+			console.log(error)
 
 		}
+
 	}
 
 
