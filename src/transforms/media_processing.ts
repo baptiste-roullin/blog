5ff1cc7e87c5cc4bdd881d66373c6d05f2fb195c
Baptiste <baptiste@roullin.net>
2023-04-08 21:32:14 +0200

    fix images

--- src/transforms/media_processing.ts
+++ src/transforms/media_processing.ts
@@ -20,11 +20,10 @@ function reformatURL(src: string, width): string {
 			'$1-' + width + '.jpg')
 }
 
-
 export default function pictures_processing(html) {
 
 	const globalSettings = {
-		selector: ` .template-post  :not(picture) > img[src]:not([srcset]):not([src$='.svg'])`,
+		selector: `.template-post	 img[src]:not([srcset]):not([src$='.svg'])`,
 		minWidth: 400,
 		maxWidth: 1920,
 /*		fallbackWidth: 750,
