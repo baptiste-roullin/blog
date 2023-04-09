319286c8b835b885d3606c6949ea855d2cb2bd39
Baptiste <baptiste@roullin.net>
2023-04-09 16:27:43 +0200

    removing useless test

--- src/transforms/media_processing.ts
+++ src/transforms/media_processing.ts
@@ -37,17 +37,19 @@ export default function pictures_processing(html) {
 	const { document } = parseHTML(html);
 
 	[...document.querySelectorAll(globalSettings.selector)]
-		.filter((image) =>
-			!((new RegExp(globalSettings.ignore)).test(image.getAttribute('src')))
+		/*	.filter((image) => {
+
+			return !((new RegExp(globalSettings.ignore)).test(image.getAttribute('src')))
+		}
 		)
-		.filter((image) => {
-			// filter out images without a src, or not SVG, or with already a srcset
-			return (
-				image.getAttribute('src') &&
-				!image.getAttribute('src').match(/\.svg$/) &&
-				!image.getAttribute('srcset')
-			)
-		})
+			.filter((image) => {
+					// filter out images without a src, or not SVG, or with already a srcset
+					return (
+						image.getAttribute('src') &&
+						!image.getAttribute('src').match(/\.svg$/) &&
+						!image.getAttribute('srcset')
+					)
+				})*/
 		.forEach(async (image) => {
 
 			if (image.getAttribute('src').match(/\.gif$/)) {
