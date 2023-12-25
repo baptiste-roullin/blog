b56d936312b080198207b62880264bb3db0702aa
Baptiste <baptiste@roullin.net>
2023-12-25 01:07:50 +0100

    todo

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { resolve } from 'path'
+import { glob } from 'glob'
 import { defineConfig } from 'vite'
 import nunjucks from 'vite-plugin-nunjucks'
 import date from './src/filters/date_formatting.js'
 import removeMD from './src/filters/remove_MD.js'
 import meta from './src/_data/meta.js'
 
-import { glob } from 'glob'
 
+
+
+//TODO : finir chemin des scripts dans les fichiers .njk
 export default defineConfig({
 	plugins: [
 		nunjucks({
