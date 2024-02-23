76b2d5ff18e3ef624243538253bfe4284350f3c6
Baptiste <baptiste@roullin.net>
2024-02-23 22:04:25 +0100

    search effing works

--- src/assets/scripts/search_front.js
+++ src/assets/scripts/search_front.js
@@ -1,24 +1,11 @@
+import createComponent from '../../_templates/components/posts_list_item.njk'
 
 export async function search(e) {
-
 
     e.preventDefault()
     const value = e.target[0].value
     const { results } = await pagefind.search(value)
-    //const results = await pagefind.debouncedSearch(value)
 
-    /*    const results = window
-    .searchIndex
-    .search(value, {
-        bool: "OR",
-        expand: true,
-        fields: {
-            title: { boost: 8 },
-            description: { boost: 5 },
-            tags: { boost: 5 },
-            content: { boost: 2 },
-        }
-    })*/
     const noResultsEl = document.getElementById("noResultsFound")
     const container = document.querySelector('.post-wrapper')
     const postList = container.children[0]
@@ -43,8 +30,6 @@ export async function search(e) {
 
                 let { url, meta, description } = doc
 
-                const createComponent = await import('../../_templates/components/posts_list_item.njk')
-
                 const el = createComponent({
                     postListItemStyle: { complete: 'complete' },
                     post: {
@@ -68,7 +53,6 @@ export async function search(e) {
             noResultsEl.style.display = "block"
         }
     }
-
 };
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -79,10 +63,9 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("search-form")
         .addEventListener("input", async function (e) {
             if (!window.pagefind) {
-
                 const pagefind = await import(/* webpackIgnore: true */ "/pagefind/pagefind.js")
-
                 window.pagefind = pagefind
+                window.pagefind.preload(e.target.value)
             }
 
         })
