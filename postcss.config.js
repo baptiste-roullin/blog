1e915e0425344029b8bdaf5efe80f3bb7dbeb9b7
Baptiste <baptiste@roullin.net>
2024-04-26 20:30:14 +0200

    fix cache busting css

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,21 @@
-//TODO : refaire marcher
-//import * as cssnano from 'cssnano'
+
 
 //import meta from './src/_data/meta.js'
 
 //const nano = (meta.env === "production" ?  : ""),
 
 
-
+import path from "node:path"
 export default {
   plugins: {
     'postcss-import': {},
     'tailwindcss/nesting': {},
     tailwindcss: { config: './tailwind.config.js' },
     "autoprefixer": {},
-    //TODO : refaire marcher
-    //    "postcss-hash": {},
+    "postcss-hash": {
+      manifest: "./src/_data/hashes_css.json",
+      /*name: function ({ dir, name, hash, ext }) { return path.join(dir, name + '.' + hash + ext) }*/
+    },
     "cssnano": {}
   }
 }
