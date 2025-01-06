c7009a9e22c6311d7e134d52e392750f963c8e80
baptiste <>
2025-01-06 22:57:13 +0100

    shortcode lightbox

--- src/pairedShortcodes/gallery.js
+++ src/pairedShortcodes/gallery.js
@@ -10,36 +10,36 @@ export async function gallery(data) {
 	if (content.match('^<ul>')) {
 		console.log('Beware: gallery shortcode must be a flat succession of img without paragraph breaks')
 	}
-
 	const { document } = parseHTML(content)
-	document.querySelectorAll('img').forEach(async (image) => {
+
+	for await (const img of document.querySelectorAll('img')) {
 		const link = document.createElement("a")
+		const src = img.getAttribute("src")
 		const path = (
-			pathUtils.isAbsolute(image.src)
+			pathUtils.isAbsolute(src)
 				?
 				"src/"
 				:
 				pathUtils.dirname(this.page.inputPath)
 		)
-
-		const imageDimensions = await Image(pathUtils.join(path, image.src), { statsOnly: true, formats: ["webp"] })
+		const imageDimensions = await Image(pathUtils.join(path, src), { statsOnly: true, formats: ["webp"] })
 
 		setAttributes(link, {
-			"href": image.getAttribute('src'),
-			'data-pswp-width': imageDimensions.webp[0].width,
-			'data-pswp-height': imageDimensions.webp[0].height
+			"href": src,
+			"data-pswp-width": imageDimensions.webp[0].width,
+			"data-pswp-height": imageDimensions.webp[0].height
 		})
-		link.appendChild(image.cloneNode(true))
-		image.replaceWith(link)
-	})
+		link.appendChild(img.cloneNode(true))
+		link.replaceWith(img)
+	}
 
-	console.log(document.toString())
+
 
 
 	let width = ''
 	if (data.match(/\!\[/g).length > 1) {
 		width = 'container-wide'
 	}
-	return `<div class="picture-gallery ${width}">${document.toString()}</div>`
+	return `<div class="picture-gallery ${width}">${document}</div>`
 }
 
