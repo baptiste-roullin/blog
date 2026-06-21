24162d6a5210b84379128ba2ab7f7dbd0beb70d0
baptiste <>
2026-06-21 09:50:27 +0200

    revert

--- src/pairedShortcodes/gallery.ts
+++ src/pairedShortcodes/gallery.ts
@@ -1,44 +1,51 @@
 import md from '../markdown.ts'
-import posthtml from 'posthtml'
-import { render } from 'posthtml-render'
-import { parser } from 'posthtml-parser'
+import { parseHTML } from 'linkedom'
 import setAttributes from '../utils/setAttributes.ts'
 import Image from "@11ty/eleventy-img"
 import pathUtils from "node:path"
 export default async function gallery(data) {
+
+
+	/*
+   on veut obtenir :
+	div.picture-gallery
+	   a
+		   figure
+			   img
+			   caption (optional)
+	*/
 
 	let html = md.render(data)
 	if (html.match('^<ul>')) {
 		console.log('Beware: gallery shortcode must be a flat succession of img without paragraph breaks')
 	}
 
+	const { document } = parseHTML(html)
 
-	const document = render(parser(html))
-	console.log(document)
+	for await (const figure of document.querySelectorAll('figure')) {
+		const img = figure.querySelector('img')
+		const link = document.createElement("a")
+		const src = img.getAttribute("src")
+		const path = (
+			pathUtils.isAbsolute(src)
+				?
+				"src/"
+				:
+				pathUtils.dirname(this.page.inputPath)
+		)
+		const imageDimensions = await Image(pathUtils.join(path, src), { statsOnly: true, formats: ["webp"] })
 
-	const figure = document.querySelector('figure')
-	const img = figure.querySelector('img')
-	const link = document.createElement("a")
-	const src = img.getAttribute("src")
-	const path = (
-		pathUtils.isAbsolute(src)
-			?
-			"src/"
-			:
-			pathUtils.dirname(this.page.inputPath)
-	)
-	const imageDimensions = await Image(pathUtils.join(path, src), { statsOnly: true, formats: ["webp"] })
+		setAttributes(link, {
+			"href": src,
+			"data-pswp-width": imageDimensions.webp[0].width,
+			"data-pswp-height": imageDimensions.webp[0].height
+		})
 
-	setAttributes(link, {
-		"href": src,
-		"data-pswp-width": imageDimensions.webp[0].width,
-		"data-pswp-height": imageDimensions.webp[0].height
-	})
-	link.replaceChildren(figure.childNodes[0], figure.childNodes[1])
-	figure.replaceChildren(link)
-
+		link.replaceChildren(...figure.childNodes)
+		figure.replaceChildren(link)
 
 
+	}
 	let width = ''
 	if (data.match(/\!\[/g).length > 1) {
 		width = 'container-wide'
